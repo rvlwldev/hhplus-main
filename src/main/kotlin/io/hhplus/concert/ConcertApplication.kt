f7fd55e03fae5a1bd16bac0c53ca2ba40fe05160
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-10-06 19:35:06 +0900

    init project

---
+++ src/main/kotlin/io/hhplus/concert/ConcertApplication.kt
@@ -0,0 +1,11 @@
+package io.hhplus.concert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConcertApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConcertApplication>(*args)
+}
