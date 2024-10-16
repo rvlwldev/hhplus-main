70d0d0805fbacb2386169b1843069b6416f9a58f
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-10-17 00:33:05 +0900

    docs: Swagger 명세 작성

--- src/main/kotlin/io/hhplus/concert/presentation/payment/PaymentController.kt
+++ src/main/kotlin/io/hhplus/concert/presentation/payment/PaymentController.kt
@@ -2,15 +2,88 @@ package io.hhplus.concert.presentation.payment
 
 import io.hhplus.concert.presentation.payment.response.PaymentHistoryResponse
 import io.hhplus.concert.presentation.payment.response.PaymentResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "3. 결제", description = "특정 콘서트를 결제합니다.")
+interface IPaymentController {
+    @Operation(
+        summary = "결제를 진행합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    schema = Schema(implementation = PaymentResponse::class),
+                    mediaType = "application/json"
+                )],
+                description = "결제 결과를 반환합니다."
+            ),
+            ApiResponse(
+                responseCode = "406",
+                content = [Content(
+                    schema = Schema(example = """{"error": "포인트가 부족합니다. 충전 후 다시 시도해주세요."}"""),
+                    mediaType = "application/json"
+                )]
+            ),
+            ApiResponse(
+                responseCode = "408",
+                content = [Content(
+                    schema = Schema(example = """{"error": "요청 시간이 초과되었습니다"}"""),
+                    mediaType = "application/json"
+                )]
+            )
+        ]
+    )
+    fun pay(@Parameter(description = "결제 토큰") @RequestHeader("Authorization") payToken: String): Any
+
+    @Operation(
+        summary = "결제 이력을 조회합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    mediaType = "application/json",
+                    array = ArraySchema(schema = Schema(implementation = PaymentHistoryResponse::class))
+                )]
+            )
+        ]
+    )
+    fun getHistoryList(@Parameter(description = "유저 ID") @PathVariable("id") userId: Long): Any
+
+    @Operation(
+        summary = "결제를 취소합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "204",
+                description = "결제 취소 완료"
+            ),
+            ApiResponse(
+                responseCode = "404", content = [Content(
+                    schema = Schema(example = """{"error": "결제 내역을 찾을 수 없습니다."}"""),
+                    mediaType = "application/json"
+                )]
+            ),
+        ]
+    )
+    fun cancel(
+        @PathVariable("id") userId: Long,
+        @PathVariable("paymentId") paymentId: Long
+    ): Any
+}
+
 @RestController
 @RequestMapping("/payments")
-class PaymentController {
+class PaymentController : IPaymentController {
 
     @PostMapping
-    fun pay(@RequestHeader("Authorization") payToken: String) = ResponseEntity.ok()
+    override fun pay(@RequestHeader("Authorization") payToken: String) = ResponseEntity.ok()
         .body(
             PaymentResponse(
                 1,
@@ -22,7 +95,7 @@ class PaymentController {
         )
 
     @GetMapping("/users/{id}/histories")
-    fun getHistoryList(@PathVariable("id") userId: Long) = ResponseEntity.ok()
+    override fun getHistoryList(@PathVariable("id") userId: Long) = ResponseEntity.ok()
         .body(
             listOf(
                 PaymentHistoryResponse(
@@ -45,7 +118,7 @@ class PaymentController {
         )
 
     @DeleteMapping("/users/{id}/histories/{paymentId}")
-    fun cancel(
+    override fun cancel(
         @PathVariable("id") userId: Long,
         @PathVariable("paymentId") paymentId: Long
     ) = ResponseEntity.noContent()
--- src/main/kotlin/io/hhplus/concert/presentation/user/UserController.kt
+++ src/main/kotlin/io/hhplus/concert/presentation/user/UserController.kt
@@ -1,21 +1,101 @@
 package io.hhplus.concert.presentation.user
 
+import io.hhplus.concert.presentation.user.request.PointRequest
 import io.hhplus.concert.presentation.user.response.PointHistoryResponse
 import io.hhplus.concert.presentation.user.response.PointResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "1. 유저", description = "유저 및 유저 포인트 관련")
+interface IUserController {
+    @Operation(
+        summary = "유저의 포인트 조회",
+        description = "특정 유저의 포인트 정보를 반환합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    schema = Schema(implementation = PointResponse::class),
+                    mediaType = "application/json"
+                )]
+            ),
+            ApiResponse(
+                responseCode = "404", content = [Content(
+                    schema = Schema(example = """{"error": "유저를 찾을 수 없습니다."}"""),
+                    mediaType = "application/json"
+                )]
+            )
+        ]
+    )
+    fun getPoint(@Parameter(description = "유저 ID") @PathVariable("id") userId: Long): Any
+
+    @Operation(
+        summary = "유저의 포인트 사용 이력 조회",
+        description = "특정 유저의 포인트 사용 이력을 반환합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    array = ArraySchema(schema = Schema(implementation = PointHistoryResponse::class)),
+                    mediaType = "application/json"
+                )]
+            ),
+            ApiResponse(
+                responseCode = "404", content = [Content(
+                    schema = Schema(example = """{"error": "유저를 찾을 수 없습니다."}"""),
+                    mediaType = "application/json"
+                )]
+            )
+        ]
+    )
+    fun getHistoryList(@Parameter(description = "유저 ID") @PathVariable("id") userId: Long): Any
+
+    @Operation(
+        summary = "유저의 포인트 충전",
+        description = "특정 유저의 포인트를 충전합니다.",
+        requestBody = RequestBody(
+            description = "충전할 포인트 정보를 포함합니다.",
+            required = true,
+            content = [Content(schema = Schema(implementation = PointRequest::class), mediaType = "application/json")]
+        ),
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    schema = Schema(implementation = PointResponse::class),
+                    mediaType = "application/json"
+                )]
+            ),
+            ApiResponse(
+                responseCode = "404", content = [Content(
+                    schema = Schema(example = """{"error": "유저를 찾을 수 없습니다."}"""),
+                    mediaType = "application/json"
+                )]
+            )
+        ]
+    )
+    fun chargePoint(@Parameter(description = "유저 ID") @PathVariable("id") userId: Long): Any
+}
+
+
 @RestController
 @RequestMapping("/users")
-class UserController {
+class UserController : IUserController {
 
     @GetMapping("/{id}/points")
-    fun getPoint(@PathVariable("id") userId: Long) = ResponseEntity.ok()
+    override fun getPoint(@PathVariable("id") userId: Long) = ResponseEntity.ok()
         .body(PointResponse(20000L))
 
-
     @GetMapping("/{id}/points/histories")
-    fun getHistoryList(@PathVariable("id") userId: Long) = ResponseEntity.ok()
+    override fun getHistoryList(@PathVariable("id") userId: Long) = ResponseEntity.ok()
         .body(
             listOf(
                 PointHistoryResponse(1, "CHARGE", 25000),
@@ -24,6 +104,6 @@ class UserController {
         )
 
     @PatchMapping("/{id}/points/charge")
-    fun chargePoint(@PathVariable("id") userId: Long) = ResponseEntity.ok()
+    override fun chargePoint(@PathVariable("id") userId: Long) = ResponseEntity.ok()
         .body(PointResponse(10000L))
 }
