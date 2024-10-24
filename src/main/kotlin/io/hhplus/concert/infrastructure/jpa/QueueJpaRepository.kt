05cd6751c2e403b15e8b534b748ec3be7df5760f
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-10-24 22:42:18 +0900

    feat: DB 비관락 명시

--- src/main/kotlin/io/hhplus/concert/infrastructure/jpa/QueueJpaRepository.kt
+++ src/main/kotlin/io/hhplus/concert/infrastructure/jpa/QueueJpaRepository.kt
@@ -1,12 +1,19 @@
 package io.hhplus.concert.infrastructure.jpa
 
 import io.hhplus.concert.domain.queue.Queue
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface QueueJpaRepository : JpaRepository<Queue, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    fun save(queue: Queue): Queue
+
     @Query("SELECT q FROM QUEUE q WHERE q.user.id = :userId")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findByUserId(userId: Long): Optional<Queue>
 
     @Query("SELECT q FROM QUEUE q WHERE q.schedule.id = :scheduleId")
