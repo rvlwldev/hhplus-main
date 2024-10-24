1e32d693410eed161bf8cc8d1b9b90543f2e80e5
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-10-25 04:40:20 +0900

    feat: 500 에러, 인증에러 로거 추가

--- src/main/kotlin/io/hhplus/concert/presentation/ApiControllerAdvice.kt
+++ src/main/kotlin/io/hhplus/concert/presentation/ApiControllerAdvice.kt
@@ -1,6 +1,8 @@
 package io.hhplus.concert.presentation
 
 import io.hhplus.concert.core.exception.BizException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -12,13 +14,20 @@ data class ErrorResponse(val code: String = "500", val message: String = "에러
 @RestControllerAdvice
 class ApiControllerAdvice : ResponseEntityExceptionHandler() {
 
+    private val logger: Logger = LoggerFactory.getLogger(ApiControllerAdvice::class.java)
+
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
+        logger.error("Unexpected error occurred: $e.message", e)
+
         return ResponseEntity(ErrorResponse(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(BizException::class)
     fun handleIllegalArgumentException(e: BizException): ResponseEntity<ErrorResponse> {
+        if (e.status == HttpStatus.UNAUTHORIZED)
+            logger.warn("Unauthorized error occurred: $e.message", e)
+
         val response = ErrorResponse("${HttpStatus.BAD_REQUEST.value()}", e.message ?: "잘못된 요청입니다.")
         return ResponseEntity(response, HttpStatus.BAD_REQUEST)
     }
