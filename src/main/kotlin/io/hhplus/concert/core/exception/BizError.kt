86d1f8de3e56d447cfabcc7f2215960f8df9ed0c
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-10-29 01:35:22 +0900

    refactor: BizError 상수 추가 정의

--- src/main/kotlin/io/hhplus/concert/core/exception/BizError.kt
+++ src/main/kotlin/io/hhplus/concert/core/exception/BizError.kt
@@ -15,22 +15,27 @@ class BizError {
     }
 
     object Queue {
+        val NOT_FOUND = Pair(HttpStatus.NOT_FOUND, "해당 대기열을 찾을 수 없습니다.")
+        val DUPLICATED = Pair(HttpStatus.NOT_FOUND, "이미 다른 대기열 요청이 처리중 입니다.")
         val TIME_OUT = Pair(HttpStatus.BAD_REQUEST, "대기열 시간이 초과되었습니다.")
     }
 
     object Schedule {
+        val NOT_FOUND = Pair(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다.")
         val NOT_ENOUGH_AVAILABLE_SEAT = Pair(HttpStatus.BAD_REQUEST, "모든 좌석이 예약되었습니다.")
         val NOT_ALLOWED_CANCEL = Pair(HttpStatus.UNAUTHORIZED, "취소할 수 없습니다.")
     }
 
     object Seat {
-        val NOT_AVAILABLE = Pair(HttpStatus.BAD_REQUEST, "좌석을 이용할 수 없습니다.")
+        val NOT_FOUND = Pair(HttpStatus.NOT_FOUND, "좌석을 찾을 수 없습니다.")
+        val NOT_AVAILABLE = Pair(HttpStatus.BAD_REQUEST, "해당 좌석을 이용할 수 없습니다.")
         val ALREADY_RESERVED = Pair(HttpStatus.CONFLICT, "이미 예약된 좌석입니다.")
         val INVALID_SEAT_NUMBER = Pair(HttpStatus.BAD_REQUEST, "잘못된 좌석 번호입니다.")
         val NOT_ALLOW_TO_RESERVE = Pair(HttpStatus.UNAUTHORIZED, "예약할 수 없습니다. 이미 취소되었거나 결제 대기시간이 초과되었습니다.")
     }
 
     object Payment {
+        val DUPLICATED = Pair(HttpStatus.CONFLICT, "이미 진행중인 결제가 존재합니다.")
         val NOT_FOUND = Pair(HttpStatus.NOT_FOUND, "결제를 찾을 수 없습니다.")
         val NOT_ENOUGH = Pair(HttpStatus.BAD_REQUEST, "보유 포인트가 부족합니다.")
         val INVALID_AMOUNT = Pair(HttpStatus.BAD_REQUEST, "올바르지 않은 포인트 사용입니다.")
