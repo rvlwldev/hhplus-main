84dbbffb03c84d39f609cd4245b793fcc63bd201
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-11-01 05:34:39 +0900

    config: (로컬) 레디스 접속 설정 추가

---
+++ src/main/kotlin/io/hhplus/concert/core/RedisConfig.kt
@@ -0,0 +1,24 @@
+package io.hhplus.concert.core
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory("127.0.0.1", 5555)
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = RedisTemplate<String, String>()
+        template.connectionFactory = redisConnectionFactory
+        return template
+    }
+
+}
