4b484b6dcf2503ec2089394189485376af41570a
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-11-08 04:04:54 +0900

    fix: 좌석 캐시에 TTL (1일) 추가

---
+++ src/main/kotlin/io/hhplus/concert/application/payment/PaymentFacadeV2.kt
@@ -0,0 +1,38 @@
+package io.hhplus.concert.application.payment
+
+import io.hhplus.concert.application.payment.result.PaymentResult
+import io.hhplus.concert.domain.concert.ConcertService
+import io.hhplus.concert.domain.payment.PaymentService
+import io.hhplus.concert.domain.schedule.ScheduleService
+import io.hhplus.concert.domain.seat.SeatService
+import org.springframework.transaction.annotation.Transactional
+
+class PaymentFacadeV2(
+    private val concertService: ConcertService,
+    private val scheduleService: ScheduleService,
+    private val seatService: SeatService,
+    private val paymentService: PaymentService,
+) {
+    fun getPayableSeatList(scheduleId: Long): List<Long> =
+        scheduleService.getReservableList(scheduleId)
+
+    fun ready(userId: Long, scheduleId: Long, seatNumber: Long) {
+        val seat = seatService.getOrCreate(userId, seatNumber)
+        seatService.readyToReserve(seat.id, userId)
+
+        val schedule = scheduleService.get(scheduleId)
+        val concert = concertService.get(schedule.id)
+
+        return paymentService.create(userId, concert.price)
+            .run { PaymentResult(this, scheduleId, seatNumber) }
+    }
+
+    @Transactional
+    fun pay(userId: Long, scheduleId: Long, seatNumber: Long): PaymentResult {
+        val seat = seatService.getOrCreate(userId, seatNumber)
+        seatService.confirm(seat.id)
+
+        return paymentService.pay(userId)
+            .run { PaymentResult(this, scheduleId, seatNumber) }
+    }
+}
--- src/main/kotlin/io/hhplus/concert/infrastructure/implement/SeatRepositoryImpl.kt
+++ src/main/kotlin/io/hhplus/concert/infrastructure/implement/SeatRepositoryImpl.kt
@@ -5,15 +5,18 @@ import io.hhplus.concert.domain.seat.SeatRepository
 import io.hhplus.concert.infrastructure.jpa.SeatJpaRepository
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
+import java.util.concurrent.TimeUnit
 
 @Repository
 class SeatRepositoryImpl(
     private val jpa: SeatJpaRepository,
-    private val redis: RedisTemplate<String, List<Seat>>
+    private val listCache: RedisTemplate<String, List<Seat>>,
+    private val monoCache: RedisTemplate<String, Seat>,
 ) : SeatRepository {
 
     override fun save(seat: Seat): Seat =
         jpa.save(seat)
+            .also { monoCache.opsForValue().set("SEAT::$it.id", it, 1, TimeUnit.DAYS) }
 
     override fun findById(id: Long): Seat? =
         jpa.findById(id).orElse(null)
@@ -22,11 +25,11 @@ class SeatRepositoryImpl(
         jpa.findByScheduleIdAndNumber(scheduleId, number).orElse(null)
 
     override fun findAllByScheduleId(scheduleId: Long): List<Seat> {
-        val cache = redis.opsForValue().get("SEATS::$scheduleId")
+        val cache = listCache.opsForValue().get("SEATS::$scheduleId")
         if (cache != null) return cache
 
         val seats = jpa.findAllByScheduleId(scheduleId)
-        redis.opsForValue().set("SEATS::$scheduleId", seats)
+        listCache.opsForValue().set("SEATS::$scheduleId", seats, 1, TimeUnit.DAYS)
 
         return seats
     }
