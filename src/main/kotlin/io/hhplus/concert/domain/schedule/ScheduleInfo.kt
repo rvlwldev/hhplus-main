0bf9d6db6431532d29e4fc23f769827f4c702567
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-11-08 03:51:21 +0900

    chore: 도메인 변경에 따른 DTO 변경

--- src/main/kotlin/io/hhplus/concert/domain/schedule/ScheduleInfo.kt
+++ src/main/kotlin/io/hhplus/concert/domain/schedule/ScheduleInfo.kt
@@ -5,11 +5,15 @@ import java.time.LocalDateTime
 data class ScheduleInfo(
     val id: Long,
     val sttAt: LocalDateTime,
+    val sttReserveAt: LocalDateTime,
     val endAt: LocalDateTime,
+    val endReserveAt: LocalDateTime
 ) {
     constructor(schedule: Schedule) : this(
         id = schedule.id,
         sttAt = schedule.sttAt,
         endAt = schedule.endAt,
+        sttReserveAt = schedule.sttReserveAt,
+        endReserveAt = schedule.endReserveAt
     )
 }
