67a7a3dc1b7321b42b203c16bdd9a4e871b3d532
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-11-12 01:31:01 +0900

    fix: PaymentFacadeV2 적용

--- src/main/kotlin/io/hhplus/concert/application/payment/PaymentFacade.kt
+++ src/main/kotlin/io/hhplus/concert/application/payment/PaymentFacade.kt
@@ -1,71 +1,30 @@
 package io.hhplus.concert.application.payment
 
 import io.hhplus.concert.application.payment.result.PaymentResult
-import io.hhplus.concert.application.support.DistributedLocker
-import io.hhplus.concert.core.exception.BizError
-import io.hhplus.concert.core.exception.BizException
 import io.hhplus.concert.domain.concert.ConcertService
 import io.hhplus.concert.domain.payment.PaymentService
-import io.hhplus.concert.domain.payment.PaymentStatus
 import io.hhplus.concert.domain.schedule.ScheduleService
 import io.hhplus.concert.domain.seat.SeatService
-import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Service
 class PaymentFacade(
-    private val distributedLocker: DistributedLocker,
     private val concertService: ConcertService,
     private val scheduleService: ScheduleService,
     private val seatService: SeatService,
-    private val paymentService: PaymentService
+    private val paymentService: PaymentService,
 ) {
-
-    @Transactional
     fun getPayableSeatList(scheduleId: Long): List<Long> =
         scheduleService.getReservableList(scheduleId)
 
-    @Transactional
     fun ready(userId: Long, scheduleId: Long, seatNumber: Long): PaymentResult {
-        val lockKey = "reservation:lock:$scheduleId:$seatNumber"
-        var lockVal: String? = null
+        val seat = seatService.getOrCreate(userId, seatNumber)
+        seatService.readyToReserve(seat.id, userId)
 
-        val maxRetries = 10
-        val retryDelay = 1000L * 3L
-        var retryCount = 0
+        val schedule = scheduleService.get(scheduleId)
+        val concert = concertService.get(schedule.id)
 
-        while (lockVal == null && retryCount < maxRetries) {
-            lockVal = distributedLocker.tryLock(lockKey)
-
-            if (lockVal == null) {
-                val seat = seatService.get(scheduleId, seatNumber)
-                if (seat != null && seat.status !== "EMPTY")
-                    throw BizException(BizError.Seat.ALREADY_RESERVED)
-
-                retryCount++
-                Thread.sleep(retryDelay)
-            }
-        }
-
-        if (lockVal == null)
-            throw BizException(BizError.Payment.ALREADY_IN_PROGRESS)
-
-        try {
-            if (paymentService.getLatestByUserId(userId).status == PaymentStatus.PAID.name)
-                throw BizException(BizError.Payment.DUPLICATED)
-
-            val seat = seatService.getOrCreate(userId, seatNumber)
-            seatService.readyToReserve(seat.id, userId)
-
-            val schedule = scheduleService.get(scheduleId)
-            val concert = concertService.get(schedule.id)
-
-            return paymentService.create(userId, concert.price)
-                .run { PaymentResult(this, scheduleId, seatNumber) }
-        } finally {
-            distributedLocker.releaseLock(lockKey, lockVal)
-        }
-
+        return paymentService.create(userId, concert.price)
+            .run { PaymentResult(this, scheduleId, seatNumber) }
     }
 
     @Transactional
@@ -76,5 +35,4 @@ class PaymentFacade(
         return paymentService.pay(userId)
             .run { PaymentResult(this, scheduleId, seatNumber) }
     }
-
 }
