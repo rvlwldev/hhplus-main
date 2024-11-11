dcb74603e548d18572cbf40282a6a26ca7d4d04a
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-11-12 01:36:43 +0900

    feat: PaymentFacade 에 맞게 자리 선점 기능 추가

--- src/main/kotlin/io/hhplus/concert/presentation/payment/IPaymentController.kt
+++ src/main/kotlin/io/hhplus/concert/presentation/payment/IPaymentController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 interface IPaymentController {
 
     fun getPayableSeatList(scheduleId: Long): ResponseEntity<List<Long>>
+    fun ready(userId: Long, scheduleId: Long, seatId: Long): ResponseEntity<PaymentResponse>
     fun pay(userId: Long, scheduleId: Long, seatId: Long): ResponseEntity<PaymentResponse>
 
 }
--- src/main/kotlin/io/hhplus/concert/presentation/payment/PaymentController.kt
+++ src/main/kotlin/io/hhplus/concert/presentation/payment/PaymentController.kt
@@ -3,6 +3,7 @@ package io.hhplus.concert.presentation.payment
 import io.hhplus.concert.application.payment.PaymentFacade
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestAttribute
@@ -17,6 +18,15 @@ class PaymentController(private val facade: PaymentFacade) : IPaymentController 
     override fun getPayableSeatList(@RequestAttribute("scheduleId") scheduleId: Long) =
         facade.getPayableSeatList(scheduleId)
             .run { ResponseEntity.ok(this) }
+
+    @PatchMapping("/seats/{seatId}")
+    override fun ready(
+        @RequestAttribute("userId") userId: Long,
+        @RequestAttribute("scheduleId") scheduleId: Long,
+        @PathVariable("seatId") seatId: Long
+    ) = facade.ready(userId, scheduleId, seatId)
+        .run { PaymentResponse(this) }
+        .run { ResponseEntity.ok(this) }
 
     @PostMapping("/seats/{seatId}")
     override fun pay(
